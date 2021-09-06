809afb3b7b188a36c3209e12cd3c42207de4fe46
“zyhon96” <adedoyin27@gmail.com>
2021-09-06 18:32:36 +0100

    Navbar sept 6 update

---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import logo from '../images/NGTeco Logo.jpg'
+import profilelogo from '../images/Group 5750.png'
+import Badge from '@material-ui/core/Badge';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { makeStyles } from '@material-ui/core/styles';
+import SearchAppBar from '../utility/SearchAppBar'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+const useStyles = makeStyles((theme) => ({
+  root:
+  {
+    margin: theme.spacing(1),
+  },
+}));
+
+function Navbar() {
+  const classes = useStyles();
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src={logo} alt="Zteco logo" />
+        <a href="_#">Test Project</a>
+      </div>
+
+      <div className="search">
+        <SearchAppBar />
+      </div>
+          <div className="search2">
+        <div className={classes.root}>
+          <Badge badgeContent={21} color="secondary" >
+            <NotificationsIcon />
+          </Badge>
+        </div>
+      </div>
+
+      <div className="last">
+        <div className="image-profile">
+          <img src={profilelogo} alt="profile-logo" />
+        </div>
+        <div className="name-profile">
+          <p>Hi, Sarah Cody</p>
+        </div>
+        <div className="dropdown">
+          <ExpandMoreIcon />
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Navbar
---
+++ src/utility/SearchAppBar.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import InputBase from '@material-ui/core/InputBase';
+import { alpha, makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'rgba(130, 130, 130, 1)',
+
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 1),
+    // vertical padding + font size from searchIcon
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    fontFamily: 'Nunito',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          placeholder="search…"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput,
+          }}
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </div>
+    </div>
+  );
+}
